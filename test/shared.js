0f4fb7245903f78cb975e558e3f34b0916d82acf
Marcello Bastea-Forte <marcello@cellosoft.com>
2013-02-24 09:32:15 -0500

    fix all the bugs!

--- test/shared.js
+++ test/shared.js
@@ -2,12 +2,13 @@ var assert = require('assert')
 
 module.exports = function(database, test) {
     for (var i=0; i<database.tests.length; i++) {
-        var testSpec = database.tests[i]
-        var fn = testSpec.exec
-        if (typeof fn === "function") {
-            test(testSpec.name.replace(/^<a href="(.+)">(.+)<\/a>$/, "$2 - $1"), function() {
-                assert.ok(description.exec(), fn.toString().replace(/\n/g, ' '))
-            })
-        }
+        (function(testSpec) {
+            var fn = testSpec.exec
+            if (typeof fn === "function") {
+                test(testSpec.name.replace(/^<a href="(.+)">(.+)<\/a>$/, "$2 - $1"), function() {
+                    assert.ok(fn(), "failed: "+fn.toString())
+                })
+            }
+        })(database.tests[i])
     }
 }
